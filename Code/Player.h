17022753f243e3e6a68c98d6cb6b36c73ac0e663
Joseph <josephc8464@gmail.com>
2026-03-07 22:23:01 -0700

    Implemented Basic Classes

---
+++ Code/Player.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER
+#define PLAYER
+
+#include <iostream>
+#include <string>
+
+class Player {
+    public:
+        //getters and setters
+    
+    private:
+        std::string name; //name of player
+
+
+}
+#endif
---
+++ Code/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using std::cout, std::endl;
+
+void main() {
+
+    
+}
