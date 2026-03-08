06ebf292b57e833b0f84de6d3b0ccdd533fc81eb
Joseph <josephc8464@gmail.com>
2026-03-08 15:12:14 -0700

    Added documentation to card

--- Code/Card.h
+++ Code/Card.h
@@ -3,18 +3,28 @@
 
 #include <string>
 /*
-*
+* @brief Holds the information of a card, including its gem color, points, cost, crowns, and ability.
+* 
+* In Splendor Duel, there are two types of cards, tier cards and royals.
+* 
+* - Tier cards can have "gemAdditions" which add permanent tokens in the player's inventory.
+* Tier cards can also have abilities or points that are used by the player to help them reach the three win conditions.
+* 
+- Royals have "crowns" and abilities that can be used when the player has enough crowns to get the royal card.
+* 
+* @author: Joseph Corella
+* @date: 2026-03-08
 */
 
 
 class Card {
     public:
-        enum class GemColor { Red, Green, Blue, White, Black}; //Gem Color
-        int gemAddition; //Number of permanent gems the card provides
-        int points; //Number of points the card provides
-        int cost[6]; //Cost of the card
-        int crowns; //Number of crowns the card provides
-        enum class ability {None, StealAToken, GetPrivelege, TakeAToken, TakeAnotherTurn, BonusJewel}; //Ability of the card
+        enum class GemColor { Red, Green, Blue, White, Black}; 
+		int gemAddition; //The number of permanent gems the player aquires when they purchase the card
+        int points;
+		int cost[6]; //Each token has an index in the cost array, which corresponds to the 5 gems and pearl
+        int crowns
+        enum class ability {None, StealAToken, GetPrivelege, TakeAToken, TakeAnotherTurn, BonusJewel};
 };
 
 #endif
